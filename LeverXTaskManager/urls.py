2cfbb25871c2ddcfa1e07a41bf1ead06c15c54cc
Aleh Zaksheuski <aleh.zaksheuski@qweeco.com>
2017-02-26 21:45:43 +0300

    #Dev. Add django settings for heroku

--- LeverXTaskManager/urls.py
+++ LeverXTaskManager/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from application.views import BasicView
 
 urlpatterns = [
+    url(r'^$', BasicView.as_view(template_name='index.html'), name='main'),
     url(r'^api/', include("application.urls.api", namespace='api')),
     url(r'^admin/', admin.site.urls),
 ]
