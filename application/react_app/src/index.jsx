2b988d70da520ed46a253555818979e5e1a17930
Aleh Zaksheuski <aleh.zaksheuski@qweeco.com>
2017-02-26 21:53:19 +0300

    #Dev. Temp commit.

--- application/react_app/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import { getAuthToken } from '../src/utils/authUtils';
-import { initApplication, initLogin } from '../src/utils/initialUtils';
-
-if (getAuthToken()) {
-  initApplication();
-} else {
-  initLogin();
-}
